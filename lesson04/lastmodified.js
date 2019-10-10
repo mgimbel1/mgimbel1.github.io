d04f9c1702b86fea4196b9f3d5ac71f4051f5a8d
mgimbel1 <55330006+mgimbel1@users.noreply.github.com>
2019-10-10 12:11:09 -0700

    Update lastmodified.js

--- lesson04/lastmodified.js
+++ lesson04/lastmodified.js
@@ -1,5 +1,1 @@
-
-var d = new Date();
-var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-var month = ["January","February","March","April","May","June","July","August","September","October","November", "December"];
-document.getElementById("datetoday").innerHTML = (days[d.getDay()] + ", " + d.getDate() + " " +month[d.getMonth()]+ " " + d.getFullYear());
+document.getElementById("lastMod").innerHTML = ( "Last Updated: " + document.lastModified);
