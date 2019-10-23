4aa698c2d14854f66fd4462c2e86c0cfe4f0cc2f
mgimbel1 <55330006+mgimbel1@users.noreply.github.com>
2019-10-23 15:25:43 -0700

    Update windchilltemp.js

--- Lesson06/windchilltemp.js
+++ Lesson06/windchilltemp.js
@@ -1,13 +1,12 @@
-/*My Function*/ 
 function doInputOutput () 
 {
 var temp a = parseFloat(document.getElementById('temp').value);
-var temp b = parseFloat(document.getElementById('windSpeed').value);
-var wc = windChill (temp a, temp b)
+var speed b = parseFloat(document.getElementById('windSpeed').value);
+var wc = wind chill ( temp a, speed b)
 document.getElementById ('output').innerHTML = wc;
 }
-function windChill (tempF,speed) 	
+function windChill (tempF,speedb) 	
 {
-var temp a = Math.pow(speed,.16);
-return result = (35.74 + (.6215 * tempF) - (35.75* a) + (.4275 * tempF * a)).toFixed(2) + " &#8457; ";
+var temp a = Math.pow(speedb,.16);
+return result = (35.74 + (.6215 * tempF) - (35.75* temp a) + (.4275 * tempF * temp a)).toFixed(2) + " &#8457; ";
 }
