956528a2b7f846795bb38e437bc17cd98ada7aaf
mgimbel1 <55330006+mgimbel1@users.noreply.github.com>
2019-11-01 16:47:47 -0700

    Create imageLoad.js

---
+++ lesson07/imageLoad.js
@@ -0,0 +1,30 @@
+const imagesToLoad = document.querySelectorAll('img[data-src]');
+const config = {
+    rootMargin: '0px 0px 150px 0px',
+    threshold: .5
+};
+const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {
+        image.removeAttribute('data-src');
+    };
+};
+
+
+if ('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+            if (item.isIntersecting) {
+                loadImages(item.target);
+                observer.unobserve(item.target);
+            }
+        });
+    }, config);
+    imagesToLoad.forEach((img) => {
+        observer.observe(img);
+    });
+} else {
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+    });
+}
