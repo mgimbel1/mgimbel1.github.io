89c35a89993a0061fd701813efc92812fe2366ea
mgimbel1 <55330006+mgimbel1@users.noreply.github.com>
2019-11-16 11:28:12 -0800

    Update home.js

--- lesson09/home.js
+++ lesson09/home.js
@@ -1,65 +1,50 @@
-var imgArray = new Array();
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
 
-imgArray[0] = new Image();
-imgArray[0].src = 'franklin.jpg';
-
-imgArray[1] = new Image();
-imgArray[1].src = 'fishhaven.jpg';
-
-imgArray[2] = new Image();
-imgArray[2].src = 'greenville.jpg';
-
-imgArray[3] = new Image();
-imgArray[3].src = 'placerton.jpg';
-
-imgArray[4] = new Image();
-imgArray[4].src = 'preston.jpg';
-
-imgArray[5] = new Image();
-imgArray[5].src = 'sodaspring.jpg';
-
-const requestURL = "https://byui-cit230.github.io/weather/data/towndata.json";
-fetch(requestURL, imgArray)
+fetch(requestURL)
   .then(function (response) {
-  return response.json();
+    return response.json();
   })
-.then(function (jsonObject) {
-console.table(jsonObject);  // temporary checking for valid response and data parsing
-const towns = jsonObject['towns'];
-for (let i = 0; i < towns.length; i++ ) {
-let card = document.createElement('section');
-let info = document.createElement('section');
+  .then(function (jsonObject) {
+    //console.table(jsonObject);  // temporary checking for valid response and data parsing
 
+    const towns = jsonObject['towns'];
+
+    for (let i = 0; i <towns.length; i++ ) {
+        if (towns[i].name == "Fish Haven" || towns[i].name == "Preston" || towns[i].name == "Soda Springs") { 
+        
+        let town = document.createElement('section');
+        let data = document.createElement('div');
         let h2 = document.createElement('h2');
-        let p1 = document.createElement('i');
+        let h3 = document.createElement('h3');
         let p2 = document.createElement('p');
         let p3 = document.createElement('p');
         let p4 = document.createElement('p');
-           let div = document.createElement('div');
         let image = document.createElement('img');
 
 
-       
-
-       
         h2.textContent = towns[i].name;
-        p1.textContent = "Our Motto: " + towns[i].motto;
+        h3.textContent = towns[i].motto;
         p2.textContent = "Year Founded: " + towns[i].yearFounded;
-        p3.textContent = "Population: " + towns[i].currentPopulation; 
-        p4.textContent ="Annual Rain Fall: " + towns[i].averageRainfall;
-         image.setAttribute('src', imgArray[i].src);
-           image.setAttribute('alt', towns[i].name + ' ' + 'sample ' + "image only");
+        p3.textContent = "Current Population: " + towns[i].currentPopulation;
+        p4.textContent = "Average Rainfall: " + towns[i].averageRainfall + " inches";
+        image.setAttribute('src', "images/" + towns[i].photo);
+        image.setAttribute('alt', "photo of "+ towns[i].name);
+        image.setAttribute('class', 'townimage');
+        data.setAttribute('class', 'towndata');
 
-            card.appendChild(info);
-  info.appendChild(h2);
-        info.appendChild(p1);
-        info.appendChild(p2);
-         info.appendChild(p3);
-        info.appendChild(p4);
-         card.appendChild(div);
-         div.appendChild(image);
+        data.appendChild(h2);
+        data.appendChild(h3);
+        data.appendChild(p2);
+        data.appendChild(p3);
+        data.appendChild(p4);
+        town.appendChild(data);
+        town.appendChild(image)
 
-      
-document.querySelector('section.cards').appendChild(card);
-} 
-});
+        document.querySelector('div.towns').appendChild(town);
+        
+    }
+    }
+
+  });
+
+  
