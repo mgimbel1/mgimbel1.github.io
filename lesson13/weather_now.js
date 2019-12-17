03c57ee3d4241b64ec50f7760ea620d1665a5f35
mgimbel1 <55330006+mgimbel1@users.noreply.github.com>
2019-12-17 09:53:24 -0800

    Update and rename weathernow.js to weather_now.js

--- lesson13/weather_now.js
+++ lesson13/weather_now.js
@@ -1,5 +1,5 @@
 **const weatherObject=XMLHttpRequest();
-weatherObject.open("GET","//api.openweathermap.org/data/2.5/weather?zip=89015,us&appid="1787f66fdf83528a7652ed44f9ac908d",true);
+weatherObject.open("GET","//api.openweathermap.org/data/2.5/weather?zip=89015,us&appid="210c6be8650140bd61b5b896bccb52f1",true);
 weatherObject.send();
 weatherObject.onload = function () {
 let weatherinfo = JSON.parse (weatherObject.responsetext);
