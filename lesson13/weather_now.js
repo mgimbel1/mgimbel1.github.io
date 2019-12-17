5a63ca24644e7ea2723c8fd8bf3140bd8c59d4b7
mgimbel1 <55330006+mgimbel1@users.noreply.github.com>
2019-12-17 09:58:26 -0800

    Update weather_now.js

--- lesson13/weather_now.js
+++ lesson13/weather_now.js
@@ -1,5 +1,5 @@
 **const weatherObject=XMLHttpRequest();
-weatherObject.open("GET","//api.openweathermap.org/data/2.5/weather?zip=89015,us&appid="210c6be8650140bd61b5b896bccb52f1",true);
+weatherObject.open("GET","//api.openweathermap.org/data/2.5/weather?zip=89015,us&appid="210c6be8650140bd61b5b896bccb52f1&units=imperial",true);
 weatherObject.send();
 weatherObject.onload = function () {
 let weatherinfo = JSON.parse (weatherObject.responsetext);
